5f53ef62266a664cef047c7e0e3f184863920946
roll20plusplus <103390875+roll20plusplus@users.noreply.github.com>
2023-04-07 19:38:41 -0700

    add list models to the ui

--- pyqtui/creature_editor_app.py
+++ pyqtui/creature_editor_app.py
@@ -6,10 +6,14 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 import statblockdatastructs
+from statblockdatastructs import AbilityScores
 
 from PyQt6.QtWidgets import (
     QApplication, QDialog, QMainWindow, QMessageBox, QLabel, QLineEdit, QListView, QSizePolicy, QComboBox, QPushButton, QCheckBox, QTextEdit
 )
+
+from PyQt6.QtGui import QStandardItemModel, QStandardItem
+
 from PyQt6 import QtCore
 
 from creature_editor_ui import Ui_Form
@@ -22,7 +26,21 @@ class CreatureEditorForm(QDialog, Ui_Form):
         super().__init__(parent)
         self.setupUi(self)
         QtCore.QDir.addSearchPath('images', os.fspath(CURRENT_DIRECTORY / "images"))
+
+        # Set up the list models for the various QListViews
+        self.save_list_model = QStandardItemModel()
+        self.skills_list_model = QStandardItemModel()
+        self.condition_list_model = QStandardItemModel()
+        self.damage_list_model = QStandardItemModel()
+        self.abilities_list_model = QStandardItemModel()
+        self.actions_list_model = QStandardItemModel()
+        self.reactions_list_model = QStandardItemModel()
+        self.bonus_actions_list_model = QStandardItemModel()
+        self.legendary_actions_list_model = QStandardItemModel()
+        self.mythic_actions_list_model = QStandardItemModel()
+
         self.setup_comboboxes()
+        self.setup_list_models()
         self.set_stylesheet()
         self.setup_checkbox_signals()
         self.init_creature_data(monster_block)
@@ -71,6 +89,7 @@ class CreatureEditorForm(QDialog, Ui_Form):
             self.size_combobox.addItem(size.name)
         for cr in statblockdatastructs.CR_TO_XP_TABLE:
             self.challenge_rating_combobox.addItem(str(cr))
+            self.challenge_rating_combobox.setCurrentIndex(0)
         for save in statblockdatastructs.AbilityScores:
             self.saving_throws_combobox.addItem(save.name)
         for skill in statblockdatastructs.SKILL_LIST:
@@ -103,14 +122,22 @@ class CreatureEditorForm(QDialog, Ui_Form):
 
 
     def init_creature_data(self, creature):
+        def initsaves():
+            # if creature.strsave: self.save_listview
+            if creature.dexsave: self.save_list_model.appendRow(QStandardItem(AbilityScores.DEXTERITY.value))
+            if creature.consave: self.save_list_model.appendRow(QStandardItem(AbilityScores.CONSTITUTION.value))
+            if creature.intsave: self.save_list_model.appendRow(QStandardItem(AbilityScores.INTELLIGENCE.value))
+            if creature.wissave: self.save_list_model.appendRow(QStandardItem(AbilityScores.WISDOM.value))
+            if creature.chasave: self.save_list_model.appendRow(QStandardItem(AbilityScores.CHARISMA.value))
+
+
         print(creature.name)
         self.name_edit.setText(creature.name)
         self.size_combobox.setCurrentIndex(self.size_combobox.findData(creature.size.value))
         self.type_edit.setText(creature.type)
         self.tag_edit.setText(creature.tag)
         self.alignment_edit.setText(creature.alignment)
-        self.challenge_rating_combobox.setCurrentIndex(self.challenge_rating_combobox.findData(creature.challengerating))
-        self.xp_calculation_label.setText(str(statblockdatastructs.CR_TO_XP_TABLE[creature.challengerating]))
+        set_combo_box_selected_item(self.challenge_rating_combobox, str(creature.challengerating))
         self.proficiency_bonus_calculation_label.setText(str(statblockdatastructs.get_prof_bonus(creature.challengerating)))
         self.hit_points_edit.setText(str(creature.hitpoints))
         self.max_hit_dice_edit.setText(creature.hitdice)
@@ -119,6 +146,23 @@ class CreatureEditorForm(QDialog, Ui_Form):
         self.armor_type_edit.setText(creature.acdesc)
         self.senses_edit.setText(creature.senses)
         self.speeds_edit.setText(creature.speed)
+        self.str_edit.setText(str(creature.ability_scores[AbilityScores.STRENGTH]))
+        self.dex_edit.setText(str(creature.ability_scores[AbilityScores.DEXTERITY]))
+        self.con_edit.setText(str(creature.ability_scores[AbilityScores.CONSTITUTION]))
+        self.int_edit.setText(str(creature.ability_scores[AbilityScores.INTELLIGENCE]))
+        self.wis_edit.setText(str(creature.ability_scores[AbilityScores.WISDOM]))
+        self.cha_edit.setText(str(creature.ability_scores[AbilityScores.CHARISMA]))
+        initSaves()
+
+
+def set_combo_box_selected_item(combo_box, item):
+    index = -1
+    for i in range(combo_box.count()):
+        if combo_box.itemText(i) == item:
+            index = i
+            break
+
+    combo_box.setCurrentIndex(index)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
