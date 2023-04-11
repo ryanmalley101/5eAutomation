3b20d89134ef58de68b8e05c49a4bc1c934bdbb9
roll20plusplus <103390875+roll20plusplus@users.noreply.github.com>
2023-04-10 22:08:55 -0700

    Conversion to GMBinder for subclasses and the excel file I'm storing them in is up and running.

--- gmbinderconvert/convert_pc.py
+++ gmbinderconvert/convert_pc.py
@@ -1,2 +1,71 @@
 from playercharacters.pc_datastructs import *
+import pandas as pd
+import numpy as np
 
+def convert_subclass(subclass:Subclass):
+    def ordinal(n: int):
+        if 11 <= (n % 100) <= 13:
+            suffix = 'th'
+        else:
+            suffix = ['th', 'st', 'nd', 'rd', 'th'][min(n % 10, 4)]
+        return str(n) + suffix
+
+    subclass_string =  f"### {subclass.name}\n"
+    subclass_string += f"{subclass.description}\n\n"
+    for ability in subclass.abilities:
+        subclass_string += f"#### {ability.name}\n"
+        subclass_string += f"*{ordinal(ability.class_level)}-level {subclass.name} feature*\n"
+        subclass_string += f"___\n"
+        subclass_string += f"{ability.description}\n\n"
+
+    return subclass_string
+
+def convert_subclass_excel():
+    subclass_excel = pd.read_excel('Subclasses.xlsx', sheet_name='Sheet1', header=0)
+    subclass_excel = subclass_excel.replace(np.nan, None)
+    subclass_list = []
+    for index, row in subclass_excel.iterrows():
+        subclass_name = row['Name']
+        class_name = ClassName(row['Class'])
+        description = "" if row['Description'] is None else row['Description']
+        ability_list = []
+        if row['Feature 1A Name'] is not None:
+            ability_list.append({"name": row['Feature 1A Name'], "description": row['Feature 1A Description'], "level": 0})
+        if row['Feature 1B Name'] is not None:
+            ability_list.append({"name": row['Feature 1B Name'], "description": row['Feature 1B Description'], "level": 0})
+        if row['Feature 2A Name'] is not None:
+            ability_list.append(
+                {"name": row['Feature 2A Name'], "description": row['Feature 2A Description'], "level": 1})
+        if row['Feature 2B Name'] is not None:
+            ability_list.append(
+                {"name": row['Feature 2B Name'], "description": row['Feature 2B Description'], "level": 1})
+        if row['Feature 3A Name'] is not None:
+            ability_list.append(
+                {"name": row['Feature 3A Name'], "description": row['Feature 3A Description'], "level": 2})
+        if row['Feature 3B Name'] is not None:
+            ability_list.append(
+                {"name": row['Feature 3B Name'], "description": row['Feature 3B Description'], "level": 2})
+        if row['Feature 4A Name'] is not None:
+            ability_list.append(
+                {"name": row['Feature 4A Name'], "description": row['Feature 4A Description'], "level": 3})
+        if row['Feature 4B Name'] is not None:
+            ability_list.append(
+                {"name": row['Feature 4B Name'], "description": row['Feature 4B Description'], "level": 3})
+        if row['Feature 5A Name'] is not None:
+            ability_list.append(
+                {"name": row['Feature 5A Name'], "description": row['Feature 5A Description'], "level": 4})
+        if row['Feature 5B Name'] is not None:
+            ability_list.append(
+                {"name": row['Feature 5B Name'], "description": row['Feature 5B Description'], "level": 4})
+        subclass = Subclass.convert_excel_template_subclass(subclass_name, class_name, description, ability_list)
+        subclass_list.append(subclass)
+
+    last_subclass = ClassName.UNKNOWN
+    for subclass in subclass_list:
+        if last_subclass != subclass.associated_class:
+            print(f'## {subclass.associated_class.value}\n')
+            last_subclass = subclass.associated_class
+        print(convert_subclass(subclass))
+
+if __name__ == '__main__':
+    convert_subclass_excel()
--- playercharacters/pc_datastructs.py
+++ playercharacters/pc_datastructs.py
@@ -4,7 +4,7 @@ import json
 from srd.srd_datastructs import AbilityDescription, CreatureStatblock, proficiency_bonus
 
 
-class ClassNames(Enum):
+class ClassName(Enum):
     ARTIFICER = "Artificer"
     BARBARIAN = "Barbarian"
     BARD = "Bard"
@@ -19,6 +19,23 @@ class ClassNames(Enum):
     WARLOCK = "Warlock"
     WIZARD = "Wizard"
     UNKNOWN = "Unknown"
+
+
+SubclassLevels = {
+    ClassName.ARTIFICER: [3, 5, 9, 15],
+    ClassName.BARBARIAN: [3, 6, 10, 14],
+    ClassName.BARD: [3, 6, 14],
+    ClassName.CLERIC: [1, 2, 6, 8, 17],
+    ClassName.DRUID: [2, 6, 10, 14],
+    ClassName.FIGHTER: [3, 7, 10, 15],
+    ClassName.MONK: [3, 6, 11, 17],
+    ClassName.PALADIN: [3, 7, 15, 20],
+    ClassName.RANGER: [3, 7, 11, 15],
+    ClassName.ROGUE: [3, 9, 13, 17],
+    ClassName.SORCERER: [1, 6, 14, 18],
+    ClassName.WARLOCK: [1, 6, 10, 14],
+    ClassName.WIZARD: [2, 6, 10, 14]
+}
 
 
 @dataclass
@@ -37,7 +54,7 @@ class Subclass:
     class SubclassAbility(AbilityDescription):
         class_level: int = 0
 
-    associated_class: ClassNames.UNKNOWN
+    associated_class: ClassName.UNKNOWN
     name: str = "PLACEHOLDER SUBCLASS"
     description: str = "This placeholder subclass is terrible, don't pick it"
     abilities: list = field(default_factory=list)
@@ -49,6 +66,19 @@ class Subclass:
 
     def to_json(self):
         return json.dumps(self.to_dict())
+
+    @staticmethod
+    def convert_excel_template_subclass(subclass_name:str, associated_class:ClassName, subclass_description:str, subclass_abilities:list):
+        new_subclass = Subclass(name=subclass_name, associated_class=associated_class, description=subclass_description, abilities=[])
+        new_subclass.name = subclass_name
+        new_subclass.associated_class = associated_class
+        new_subclass.description = subclass_description
+        for ability in subclass_abilities:
+            level = SubclassLevels[associated_class][ability['level']]
+            subclass_ability = Subclass.SubclassAbility(name=ability['name'], description=ability['description'], class_level=level)
+            new_subclass.abilities.append(subclass_ability)
+        return new_subclass
+
 
 
 @dataclass
